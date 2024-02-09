1710f142daa077ff8e9e2a26fedb2e353678fcc7
Nilsu Derinder <44870116+exababy@users.noreply.github.com>
2024-02-09 07:22:42 +0300

    Update vip_rm.cpp

--- VIP_RainbowModel/source/vip_rm.cpp
+++ VIP_RainbowModel/source/vip_rm.cpp
@@ -153,9 +153,14 @@ bool vip_rm::Unload(char *error, size_t maxlen)
 	return true;
 }
 
+CGameEntitySystem* GameEntitySystem()
+{
+    return g_pVIPCore->VIP_GetEntitySystem();
+};
+
 void VIP_OnVIPLoaded()
 {
-	g_pGameEntitySystem = g_pVIPCore->VIP_GetEntitySystem();
+	g_pGameEntitySystem = GameEntitySystem();
 	g_pEntitySystem = g_pGameEntitySystem;
 }
 
